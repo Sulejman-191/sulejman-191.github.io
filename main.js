34c40d2a3c896dd94292993965ec7ef13062aa38
Luka Šulc <lukasulc0@gmail.com>
2023-03-15 23:24:45 +0100

    v1.0

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
+cities = [
+    "Sevilla",
+    "Zagreb",
+    "Minsk",
+    "Berlin"
+]
+
+defaultData = {
+    "temperature": "29 °C",
+    "wind": "20 km/h",
+    "description": "Partly cloudy",
+    "forecast": [
+        {
+            "day": 1,
+            "temperature": "27 °C",
+            "wind": "12 km/h"
+        },
+        {
+            "day": 2,
+            "temperature": "22 °C",
+            "wind": "8 km/h"
+        }
+    ]
+}
+
+
 async function fetchWeather(city = 'Zagreb') {
+    // fetch('./countries.json').then(res => console.log(res))
     return await fetch("https://goweather.herokuapp.com/weather/" + city)
-        .then((response) => response.json())
+        .then((response) => response.json()).catch(onrejected => defaultData)
 
 }
 
@@ -9,7 +36,17 @@ function printWeather(text) {
 }
 
 function main() {
-    fetchWeather().then(data => printWeather(data))
+    for (city of cities) {
+
+        fetchWeather(city).then(data => {
+            let cointainer = document.getElementById('weather-container')
+            let newDiv = document.createElement('div')
+            // newDiv.className = ''
+            console.log(data);
+            newDiv.innerText = String(city) + ": " + data.temperature + " " + data.wind + " " + data.description + " | "
+            cointainer.appendChild(newDiv)
+        })
+    }
 }
 
 main()
